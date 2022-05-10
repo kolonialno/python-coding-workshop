ab02f768e98b9061ff8fe1ae9fc34a2463b05111
Marie Kjellstrøm Thorkildsen <marie.thorkildsen@kolonial.no>
2022-05-10 10:36:53 +0200

    Translate task1.py

--- task1.py
+++ task1.py
@@ -1,48 +1,70 @@
 """
-OPPGAVE 1: VARIABLER OG IF-SETNING
+TASK 1: VARIABLES AND IF STATEMENTS
 
-Vi starter med å definere to variabler som vi kaller x og y.
-Om du vil kan du legge inn dine egne verdier (for eksempel bytte ut 10 med ditt eget tall).
+First, we define two variables called x and y. If you want, you can change
+the values to something else, for example by replacing 10 with another number.
 """
 
 x = 10
 y = 20
 
 """
-La oss sjekke at variablene har fått tilordnet verdiene vi valgte. Vi bruker print-funksjonen for at terminalen (det sorte vinduet til høyre) skal vise oss hva den har lagret. Fjern # foran "print(x)" og trykk "Run", eventuelt Ctrl+Enter (eller Cmd+Enter for Mac).
+Let's check that the values we chose above are assigned to the variables. We use
+the print method to check this, and the values will appear in the terminal (the
+black window to the right).
+
+Remove the # in front of print(x) and press "Run" (alternatively Ctrl+Enter on
+Windows or Cmd+Enter on Mac). 
 """
+
 #print(x)
 
 """
-Vi kan også legge til mer tekst for å enklere se hva terminalen har lagret. Fjern igjen # og trykk "Run".
-"""
-#print("x har verdien:",x)
-
-
-# A: Skriv kode for å sjekke om y også har fått verdien du forventer. Kjør, og sjekk at verdien til y stemmer.
-
-#print(y)
-
-# B: Spør sidekvinnen hvor gammel hun er og lagre både hennes og din alder i hver sine variabler
-
-#minAlder =
-#sidekvinneAlder =
-
-"""
-Vi skal nå lage din første if-setning! Vi vil at terminalen skal printe hvem som er eldst. 
-Først, se på koden nedenfor og finn ut hva du forventer skjer når du kjører koden. Fjern de tre hermetegnene rundt if-setningen og trykk Run.
+We can also add more text to the print method to make the output easier to read.
+Again, remove the # and press "Run".
 """
 
+#print("x has the following value:", x)
+
 """
-if minAlder > sidekvinneAlder:
-  print("Jippi, du er eldst!")
-elif sidekvinneAlder > minAlder:
-  print("Sidekvinnen er eldst")
+Write code to check that y has the value you expect. Press "Run" and verify 
+that the value is correct.
+"""
+
+# Write your code here
+
+"""
+Ask the colleague next to you about her/his age. Assign your age and the age of
+the colleague next to you to separate variables.
+"""
+
+#my_age =
+#colleague_age =
+
+"""
+You are now about to create your first if statement! We want to check who's the
+eldest – you or your colleague?
+
+First, have a look at the code below. What do you think will happen when this
+code is run?
+
+When you're done thinking, remove the three " above and below the code and run it.
+"""
+
+"""
+if my_age > colleage_age:
+  print("You are the eldest!")
+elif colleage_age > my_age:
+  print("Your colleage is the eldest!")
 else:
-  print("Dere er like gamle.")
+  print("You are the same age.")
 """
 
-tall1 = 5
-tall2 = 3
+a = 5
+b = 3
 
-# C: Lag en if-setning der du sjekker hvilket av tall1 og tall2 som er størst, og printer det største tallet.
+"""
+Make an if statement where you check whether a or b has the largest value.
+"""
+
+# Write your code here
